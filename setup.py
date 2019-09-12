e9f9f85c156328da0caeeda1f44759ea26850d07
Emmanuel Corrales <contact@emcorrales.com>
2019-09-11 21:46:20 -0700

    Create a setup script.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='Hello World!',
+      version='0.0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/emcorrales/gh-actions-pypi-deployment-demo',
+      author='Emmanuel M. Corrales',
+      author_email='contact@emcorrales.com',
+      license='MIT',
+      packages=['hello_world'],
+      zip_safe=False)
