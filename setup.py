aefd6422262d5ff4c456ab2048ad6c03271c1c16
Emmanuel Corrales <contact@emcorrales.com>
2019-09-11 23:05:45 -0700

    Rename hello_world to emc.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='hello_world',
+setup(name='emc',
       version='0.1',
       description='A simple hello world library',
       url='http://github.com/emcorrales/gh-actions-pypi-deployment-demo',
       author='Emmanuel M. Corrales',
       author_email='contact@emcorrales.com',
       license='MIT',
-      packages=['hello_world'],
+      packages=['emc'],
       zip_safe=False)
